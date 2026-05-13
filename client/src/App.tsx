df2963c0bdc5b4da00f94bc3d8f6ab7b098134b3
ikeshav26 <keshavgilhotra4@gmail.com>
2026-05-13 21:42:00 +0530

    Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
 
   return (
     <div style={{ padding: "20px", fontFamily: "system-ui" }}>
-      <h1>Welcome to the App!</h1>
+      <h1>Testing Automations</h1>
       <img src="favicon.svg" alt="Logo"/>
       <button 
         onClick={fetchData} 
